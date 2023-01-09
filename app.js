adb501ff38b543528eca5f15040d36960bb5fa39
kalpa <kalpamadhushan18@gmail.com>
2023-01-09 17:18:20 +0530

    connect DB

--- app.js
+++ app.js
@@ -15,15 +15,34 @@ app.route('/')
 //CRUD for selected coffee
 app.route('/coffee/:coffeeID')
 
+
+//connect to DB
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/coffeeDB'); // database name is coffeeDB
+}
+
+// DB schema
+const CoffeeSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  quantity : Number,
+});
+
+// DB Model
+
+
+
 //show selected coffee to the user
 .get((req,res)=>{       
     const coffeeID= req.params.coffeeID;
-    res.send(coffeeID);
+    res.send({coffee :coffeeID});
 })
 
 //buy the selected coffee
 .post((req,res)=>{     
     const coffeeID= req.params.coffeeID;
+    const quantity = req.body.quantity; // post request should include the number of cups as quantity
 })
 
 //start server on PORT 5000
